1cf25ce50a0fb2a40cbfea2166f7c76dcdf6060d
pokemium <bluejapan73+dev@gmail.com>
2022-01-05 16:25:16 +0900

    chap4

--- src/main.rs
+++ src/main.rs
@@ -724,15 +724,11 @@ impl DnsPacket {
     }
 }
 
-fn main() {
-    // google.comについてのAクエリを実行します。
-    let qname = "www.yahoo.com";
-    let qtype = QueryType::MX;
-
+fn lookup(qname: &str, qtype: QueryType) -> Result<DnsPacket> {
     // DNSサーバとしてGoogleのDNSサーバーを使います。
     let server = ("8.8.8.8", 53);
 
-    let socket = UdpSocket::bind(("0.0.0.0", 43210)).unwrap();
+    let socket = UdpSocket::bind(("0.0.0.0", 43210))?;
 
     // パケットの情報を人間にわかりやすい形で持っておく(これを後でバイト列に変換して送信)
     let mut packet = DnsPacket::new();
@@ -745,29 +741,76 @@ fn main() {
 
     let mut req_buffer = BytePacketBuffer::new();
 
-    packet.write(&mut req_buffer).unwrap();
+    packet.write(&mut req_buffer)?;
 
-    socket
-        .send_to(&req_buffer.buf[0..req_buffer.pos], server)
-        .unwrap();
+    socket.send_to(&req_buffer.buf[0..req_buffer.pos], server)?;
 
     let mut res_buffer = BytePacketBuffer::new();
-    socket.recv_from(&mut res_buffer.buf).unwrap();
+    socket.recv_from(&mut res_buffer.buf)?;
 
-    let res_packet = DnsPacket::from_buffer(&mut res_buffer).unwrap();
+    DnsPacket::from_buffer(&mut res_buffer)
+}
 
-    // ヘッダ、クエスチョン、各レコードを出力
-    println!("{:#?}", res_packet.header);
-    for q in res_packet.questions {
-        println!("{:#?}", q);
+fn handle_query(socket: &UdpSocket) -> Result<()> {
+    let mut req_buffer = BytePacketBuffer::new();
+
+    let (_, src) = socket.recv_from(&mut req_buffer.buf)?;
+
+    let mut request = DnsPacket::from_buffer(&mut req_buffer)?;
+
+    let mut packet = DnsPacket::new();
+    packet.header.id = request.header.id;
+    packet.header.recursion_desired = true;
+    packet.header.recursion_available = true;
+    packet.header.response = true;
+
+    if let Some(question) = request.questions.pop() {
+        println!("Received query: {:?}", question);
+
+        if let Ok(result) = lookup(&question.name, question.qtype) {
+            packet.questions.push(question);
+            packet.header.rescode = result.header.rescode;
+
+            for rec in result.answers {
+                println!("Answer: {:?}", rec);
+                packet.answers.push(rec);
+            }
+            for rec in result.authorities {
+                println!("Authority: {:?}", rec);
+                packet.authorities.push(rec);
+            }
+            for rec in result.resources {
+                println!("Resource: {:?}", rec);
+                packet.resources.push(rec);
+            }
+        } else {
+            packet.header.rescode = ResultCode::SERVFAIL;
+        }
+    } else {
+        packet.header.rescode = ResultCode::FORMERR;
     }
-    for rec in res_packet.answers {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.authorities {
-        println!("{:#?}", rec);
-    }
-    for rec in res_packet.resources {
-        println!("{:#?}", rec);
+
+    let mut res_buffer = BytePacketBuffer::new();
+    packet.write(&mut res_buffer)?;
+
+    // 空白部分を切り捨てる
+    let len = res_buffer.pos();
+    let data = res_buffer.get_range(0, len)?;
+
+    socket.send_to(data, src)?;
+
+    Ok(())
+}
+
+fn main() {
+    // ポート2053にUDPソケットをバインド
+    let socket = UdpSocket::bind(("0.0.0.0", 2053)).unwrap();
+
+    // 今のところ、クエリは順次処理されているので、リクエストを処理するための無限ループが発生しています。
+    loop {
+        match handle_query(&socket) {
+            Ok(_) => {}
+            Err(e) => eprintln!("An error occurred: {}", e),
+        }
     }
 }
